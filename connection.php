ee6100512c7a4abdc8a78ff0d50a07e18e146085
jsnodees <202280134@psu.palawan.edu.ph>
2024-11-26 22:05:10 +0800

    code to link it to the database(quiz_db)

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'quiz_db';
+$port = '3307';
+
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
+
+if (!$conn) {
+    die ("Connection failed: " . mysqli_connect_error());
+}
+?>
